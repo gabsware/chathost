06c0c6ffe36ce0ce47b30ae8629d240eef881c84
gabsware <gabrielziinff@gmail.com>
2026-03-23 16:08:15 -0300

    fix: puppeteer chrome

--- bot.js
+++ bot.js
@@ -12,10 +12,11 @@ const { Client, LocalAuth } = require('whatsapp-web.js');
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
-        headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
-    }
-});
+    headless: true,
+    executablePath: require('puppeteer').executablePath(),
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+}
+
 // "BANCO DE DADOS" EM MEMÓRIA
 const usuarios = {};
 
